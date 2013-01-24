258147a793273ccd3e638a40018f4b9943a3a8b0
Justin Becker (Geekman7473) <geekman7473@gmail.com>
2013-01-23 20:35:15 -0500

    Balls now move on button press

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,9 +22,60 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 50
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(13, 13)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(95, 13)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(177, 15)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightGray
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,46 @@
 ﻿Public Class Form1
+    Dim pucks As System.Drawing.Graphics
+    Dim testPuck As puck
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
 
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        pucks = Me.CreateGraphics
+    End Sub
+    Private Sub Form1_Resize(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Resize
+        pucks = Me.CreateGraphics
+    End Sub
+    Private Structure puck
+        'Private Dim draw As System.Drawing
+        Public radius As Integer
+        Public color As Brush
+        Public location As Point
+        Public pixelsPerSec As Double
+        Public direction As Double
+        Public Sub updatePos()
+            'MsgBox(location.Y & " " & location.X & " " & pixelsPerSec * Math.Cos(direction) & " " & (pixelsPerSec * Math.Sin(direction)) & " " & Math.Sqrt(Math.Pow(location.X + (pixelsPerSec * Math.Cos(direction)) - location.X, 2) + Math.Pow(location.Y + (pixelsPerSec * Math.Sin(direction)) - location.Y, 2)))
+            location.X = location.X + (pixelsPerSec * Math.Cos(direction * (Math.PI / 180)))
+            location.Y = location.Y + (pixelsPerSec * Math.Sin(direction * (Math.PI / 180)))
+        End Sub
+    Public Sub drawPuck()
+        Form1.pucks.FillEllipse(color, location.X, location.Y, radius, radius)
+    End Sub
+    End Structure
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        testPuck.radius = 15
+        testPuck.color = Brushes.Black
+        testPuck.location = New Point(50, 50)
+        testPuck.direction = 0
+        testPuck.pixelsPerSec = 5
+        testPuck.drawPuck()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        pucks.Clear(Me.BackColor)
+        If TextBox1.Text <> "" Then testPuck.direction = TextBox1.Text
+        testPuck.updatePos()
+        testPuck.drawPuck()
+    End Sub
 End Class
