2df7bcebc9f8eff75f32d4471489a3aed4a479fc
Justin Becker (Geekman7473) <geekman7473@gmail.com>
2013-01-24 13:58:33 -0500

    Pucks now 'bounce' at correct angles

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -27,6 +27,7 @@ Partial Class Form1
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Timer1
@@ -53,10 +54,19 @@ Partial Class Form1
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(177, 15)
+        Me.TextBox1.Location = New System.Drawing.Point(258, 15)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(100, 20)
         Me.TextBox1.TabIndex = 2
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(177, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
         '
         'Form1
         '
@@ -64,6 +74,7 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.LightGray
         Me.ClientSize = New System.Drawing.Size(1313, 685)
+        Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
@@ -77,5 +88,6 @@ Partial Class Form1
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents Button2 As System.Windows.Forms.Button
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,19 +1,6 @@
 ﻿Public Class Form1
     Dim pucks As System.Drawing.Graphics
     Dim testPuck As puck
-    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
-        pucks.Clear(Me.BackColor)
-        testPuck.updatePos(1000 / Timer1.Interval)
-        testPuck.drawPuck()
-    End Sub
-
-    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        pucks = Me.CreateGraphics
-        Timer1.Enabled = False
-    End Sub
-    Private Sub Form1_Resize(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Resize
-        pucks = Me.CreateGraphics
-    End Sub
     Private Structure puck
         'Private Dim draw As System.Drawing
         Public radius As Integer
@@ -28,11 +15,31 @@
             location.Y = location.Y + ((pixelsPerSec / ticksPerSec) * Math.Sin(direction * (Math.PI / 180)))
             'MsgBox(location.X & location.Y)
         End Sub
-    Public Sub drawPuck()
-        Form1.pucks.FillEllipse(color, location.X, location.Y, radius, radius)
-    End Sub
-    End Structure
+        Public Sub drawPuck()
+            Form1.pucks.FillEllipse(color, location.X, location.Y, radius, radius)
+        End Sub
+        Public Sub collisionVector()
+            direction = (direction - 2 * (direction Mod 90)) - 180
+            If direction < 0 Then
+                direction = 360 - Math.Abs(direction Mod 360)
+            Else
+                direction = direction Mod 360
+            End If
 
+        End Sub
+    End Structure
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        pucks.Clear(Me.BackColor)
+        testPuck.updatePos(1000 / Timer1.Interval)
+        testPuck.drawPuck()
+    End Sub
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        pucks = Me.CreateGraphics
+        Timer1.Enabled = False
+    End Sub
+    Private Sub Form1_Resize(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Resize
+        pucks = Me.CreateGraphics
+    End Sub
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         testPuck.radius = 15
         testPuck.color = Brushes.Black
@@ -42,11 +49,13 @@
         testPuck.drawPuck()
         Timer1.Enabled = True
     End Sub
-
     Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
         pucks.Clear(Me.BackColor)
         If TextBox1.Text <> "" Then testPuck.direction = TextBox1.Text
         testPuck.updatePos(1)
         testPuck.drawPuck()
+    End Sub
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        testPuck.collisionVector()
     End Sub
 End Class
